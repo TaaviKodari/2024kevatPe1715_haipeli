94cce576134b729a3adecc055e621a5c45628fa6
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2024-04-12 18:19:29 +0300

    enemypoolmaanger aloitettu

---
+++ 2024KevatPe1600_Haipeli/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float currentSpeed = 3f;
+    public Transform playerTransform;
+    private Rigidbody2D body;
+    private Vector2 direction;
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if(playerTransform == null){
+            return;
+        }
+        direction = (playerTransform.position - transform.position).normalized;
+        body.MovePosition(body.position + direction * currentSpeed * Time.fixedDeltaTime);
+    }
+}
