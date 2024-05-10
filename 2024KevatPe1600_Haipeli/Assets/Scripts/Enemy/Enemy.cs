03fc6009e893d07734b90f1bee0c75c20679bcf7
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2024-05-10 18:17:43 +0300

    vihollisen hyökkäys tehty

--- 2024KevatPe1600_Haipeli/Assets/Scripts/Enemy/Enemy.cs
+++ 2024KevatPe1600_Haipeli/Assets/Scripts/Enemy/Enemy.cs
@@ -8,6 +8,12 @@ public class Enemy : MonoBehaviour, IDamageable
     public float currentSpeed = 3f;
     public Transform playerTransform;
     public int maxHealth = 3;
+    public float attackRange = 10f;
+    public float attackCooldown = 2f;
+    public float dashSpeed = 20f;
+    public float dashDuration = 1f;
+    private bool isDashing = false;
+    private float attackTimer;
     private int currentHealth;
     private Rigidbody2D body;
     private Vector2 direction;
@@ -24,17 +30,55 @@ public class Enemy : MonoBehaviour, IDamageable
     void OnEnable()
     {
         currentHealth = maxHealth;
+        attackTimer = attackCooldown;
+        isDashing = false;
+    }
+
+    void OnDisable()
+    {
+        StopAllCoroutines();    
     }
 
     void FixedUpdate()
     {
         Move();
+        Attack();
+    }
+
+    void Attack()
+    {
+        if(playerTransform == null){
+            return;
+        }
+
+        if(attackTimer > 0){
+            attackTimer -= Time.fixedDeltaTime;
+        }
+        else if(!isDashing && Vector2.Distance(transform.position, playerTransform.position) < attackRange){
+            
+            StartCoroutine(DashAttack());
+        }
+    }
+
+    IEnumerator DashAttack(){
+        float startTime = Time.time;
+        isDashing = true;
+        while(Time.time < startTime + dashDuration){
+            body.velocity = direction * dashSpeed;
+            yield return null;
+        }
+        isDashing = false;
+        body.velocity = Vector2.zero;
+        attackTimer = attackCooldown;
     }
 
     void Move()
     {
         if(playerTransform == null){
             GetPlayer();
+            return;
+        }
+        if(isDashing){
             return;
         }
         direction = (playerTransform.position - transform.position).normalized;
